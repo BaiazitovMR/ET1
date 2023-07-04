e02dd2d63f6d9326eb86bbde896114f9ab89485e
BaiazitovMR <133628617+BaiazitovMR@users.noreply.github.com>
2023-07-04 12:04:45 +0500

    написал программу

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Введите элементы массива, разделенные запятой:");
+        string input = Console.ReadLine();
+        string[] inputArray = input.Split(',');
+
+        string[] newArray = FilterArray(inputArray);
+
+        Console.WriteLine("Новый массив:");
+        foreach (string item in newArray)
+        {
+            Console.WriteLine(item);
+        }
+
+        Console.ReadLine();
+    }
+
+    static string[] FilterArray(string[] inputArray)
+    {
+        int count = 0;
+        foreach (string item in inputArray)
+        {
+            if (item.Length <= 3)
+            {
+                count++;
+            }
+        }
+
+        string[] newArray = new string[count];
+        int index = 0;
+        foreach (string item in inputArray)
+        {
+            if (item.Length <= 3)
+            {
+                newArray[index] = item;
+                index++;
+            }
+        }
+
+        return newArray;
+    }
+}
